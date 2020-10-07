8695751f55b667a97ecd4fe707e656aef905647f
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-07 17:08:58 +0200

    ✏️ Rename __main__.py to telegram.py
    and drop Baron, as it's not needed anymore

--- thor/telegram.py
+++ thor/telegram.py
@@ -10,15 +10,6 @@ log = logging.getLogger(__name__)
 
 
 async def main():
-    log.info("Starting Baron...")
-    baron: royalnet.baron.Baron = royalnet.baron.Baron(redis_args={
-        "host": os.environ["REDIS_HOST"],
-        "port": os.environ["REDIS_PORT"],
-        "db": os.environ["REDIS_DB"],
-        "password": os.environ.get("REDIS_PASSWORD")
-    })
-    baron.start()
-
     log.info("Starting Alchemist...")
     alchemist: royalnet.alchemist.Alchemist = royalnet.alchemist.Alchemist(
         engine_args=[os.environ["SQLALCHEMY_DATABASE_URI"]],
